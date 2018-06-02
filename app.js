10e5654db22c543da99d64f94f83ae64bb56c96d
AbigailJuarez <abihail_goodlove@outlook.com>
2018-06-01 21:53:50 -0500

    c/s

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ var express= require('express');
 var logger= require('morgan');
 var bodyParser= require('body-parser')
 
-
-
 var app =express();
 app.set('views',path.resolve(__dirname,'views'));
 app.set('view engine','ejs');
 var entries =[];
 app.locals.entries=entries;
-
 
 var publicPath= path.join(__dirname,'public');
 app.use('/recursos',express.static(publicPath));
@@ -24,6 +21,7 @@ app.get('/index',(request,response)=> response.render('index'));
 app.get('/clase',(request,response)=> response.render('Clase'));
 app.get('/armas',(request,response)=> response.render('Armas'));
 app.get('/victimas',(request,response)=> response.render('Victimas'));
+app.get('/new-entry',(request,response)=>response.render('new-entry'));
 
 
 app.post('/new-entry',(request,response)=>{
@@ -42,3 +40,5 @@ app.use((request,response)=>response.status(404).render('404'));
 http.createServer(app).listen(3000,()=>
 console.log('corriendo en el puerto 3000')
 );
+
+
